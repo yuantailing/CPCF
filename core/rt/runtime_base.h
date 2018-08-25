edcfab1daf649c7826d365ae1932ffa379900c2f
Jiaping Wang <jiapw.cg@gmail.com>
2018-08-25 14:53:14 -0700

    !B: ObjectPlaceHolder override default copy ctor and operator =

--- core/rt/runtime_base.h
+++ core/rt/runtime_base.h
@@ -1145,18 +1145,41 @@ template<typename T>
 class ObjectPlaceHolder
 {
 	BYTE	_Place[sizeof(T)];
-	bool	_Init;
+	bool	_bInit;
+	void	_Copy(const ObjectPlaceHolder<T>& x){ if(x._bInit){ new (_Place) T(x); _bInit = true; }else{ _bInit = false; } }
+
 public:
-	ObjectPlaceHolder(){ _Init = false; }
+	ObjectPlaceHolder(){ _bInit = false; }
+	ObjectPlaceHolder(const T& x){ new (_Place) T(x); _bInit = true; }
+	ObjectPlaceHolder(const ObjectPlaceHolder<T>& x){ _Copy(x); }
+
+	const T& operator = (const T& x){ Term(); new (_Place) T(x); _bInit = true; return x; }
+	const ObjectPlaceHolder<T>& operator = (const ObjectPlaceHolder<T>& x){ Term(); _Copy(x); return x; }
+
 	~ObjectPlaceHolder(){ Term(); }
-	void Term(){ if(_Init){ _Init = false; ((T*)_Place)->~T(); } }
-	void Init(){ ASSERT(!_Init); new (_Place) T; _Init = true; }
+	void Term(){ if(_bInit){ _bInit = false; ((T*)_Place)->~T(); } }
+
+	void Init(){ ASSERT(!_bInit); new (_Place) T; _bInit = true; }
+	void Reinit(){ Term(); new (_Place) T; _bInit = true; }
+
+	void Init(const ObjectPlaceHolder<T>& x){ ASSERT(!_bInit); _Copy(x); }
+	void Reinit(const ObjectPlaceHolder<T>& x){ Term(); _Copy(x); }
+
 	template<typename... Params>
-	void Init(Params... args){ ASSERT(_Init); new (_Place) T(std::forward<Params>(args)...); _Init = true; }
-	operator T& (){ if(!_Init)Init(); return *(T*)_Place; }
-	operator const T& () const { ASSERT(_Init); return *(const T*)_Place; }
-	operator T* (){ if(!_Init)Init(); return (T*)_Place; }
-	operator const T* () const { ASSERT(_Init); return (const T*)_Place; }
+	void Init(Params... args){ ASSERT(!_bInit); new (_Place) T(std::forward<Params>(args)...); _bInit = true; }
+	template<typename... Params>
+	void Reinit(Params... args){ Term(); new (_Place) T(std::forward<Params>(args)...); _bInit = true; }
+
+	T&			Object(){ return *(T*)_Place; }
+	const T&	Object() const { return *(const T*)_Place; }
+
+	operator T& (){ if(!_bInit)Init(); return *(T*)_Place; }
+	operator const T& () const { ASSERT(_bInit); return *(const T*)_Place; }
+	operator T* (){ if(!_bInit)Init(); return (T*)_Place; }
+	operator const T* () const { ASSERT(_bInit); return (const T*)_Place; }
+
+	T* operator ->(){ ASSERT(_bInit); return (T*)_Place; }
+	const T* operator ->() const { ASSERT(_bInit); return (const T*)_Place; }
 };
 
 
